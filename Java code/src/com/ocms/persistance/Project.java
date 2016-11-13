b1439ac8668f66cec726195179f8f46159fbd0bb
Yuichiro Senzaki <yuitiro.senzaki@ruri.waseda.jp>
2016-11-14 01:03:13 +0900

    create entity classes

--- Java code/src/com/ocms/persistance/Project.java
+++ Java code/src/com/ocms/persistance/Project.java
@@ -1,5 +1,14 @@
 package com.ocms.persistance;
 
-public class Project {
+import java.util.Set;
 
+public class Project {
+	private int projectId;
+	private int prjId;
+	private String name;
+	private Set<Checklist> checklists;
+
+	public void setProjectId(int projectid){
+
+	}
 }
