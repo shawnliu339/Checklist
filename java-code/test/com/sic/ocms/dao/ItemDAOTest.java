9170a8178fe82248013c450b5b707f6ca9e4e210
SranLiu <SranLiu@Amelias-MacBook-Air.local>
2016-12-01 14:13:39 +0900

    add dao transaction.

--- java-code/test/com/sic/ocms/dao/ItemDAOTest.java
+++ java-code/test/com/sic/ocms/dao/ItemDAOTest.java
@@ -1,5 +1,23 @@
 package com.sic.ocms.dao;
 
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sic.ocms.dao.item.ItemDAO;
+import com.sic.ocms.persistence.Item;
+
 public class ItemDAOTest {
 
+	
+	@Test
+	public void testSave() throws Exception {
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		
+		ItemDAO dao = (ItemDAO) ctx.getBean("itemDAO");
+		dao.add(new Item());
+		ctx.destroy();
+		
+	}
+	
 }
